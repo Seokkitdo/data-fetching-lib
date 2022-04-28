21cc0fb6449cbc877c88d633bd04bcf2494c1437
Seokkitdo <sjd12329@gmail.com>
2022-04-28 20:50:06 +0900

    Add : useQuery study

--- src/App.js
+++ src/App.js
@@ -2,15 +2,17 @@ import Example from './reactQuery/Example';
 import { QueryClient, QueryClientProvider } from 'react-query';
 import {ReactQueryDevtools} from 'react-query/devtools'
 import QuickStart from './reactQuery/QuickStart';
+import Pagination from './reactQuery/Pagination';
 
 function App() {
   const queryClient = new QueryClient();
   return (
     <>
     <QueryClientProvider client={queryClient}>
-      <Example />
-      <QuickStart />
-      <ReactQueryDevtools />
+      {/* <Example /> */}
+      {/* <QuickStart /> */}
+      {/* <ReactQueryDevtools /> */}
+      <Pagination />
     </QueryClientProvider>
     </>
   );
--- src/mocks/handlers.js
+++ src/mocks/handlers.js
@@ -24,6 +24,29 @@ const todos = [
 ]
 
 export const handlers = [
+    rest.get("http://localhost:3000/api/projects", async(req,res,ctx) => {
+        const pageIndex = req.url.searchParams.get("page");
+        return res(
+            ctx.json({
+                projects:[
+                {
+                    id: `1 ${pageIndex}`,
+                    name: `jimmy 1-${pageIndex}`
+                },{
+                    id: `2 ${pageIndex}`,
+                    name: `jimmy 2-${pageIndex}`
+                },{
+                    id: `3 ${pageIndex}`,
+                    name: `jimmy 3-${pageIndex}`
+                },{
+                    id: `4 ${pageIndex}`,
+                    name: `jimmy 4-${pageIndex}`
+                }
+            ],
+            hasMore: pageIndex < 3,
+            })
+        )
+    }),
     rest.post("http://localhost:3000/api/todo", async(req, res ,ctx) => {
         const {todo} = req.body;
         console.log(JSON.stringify(todo));
